fd5d742aeffec4c7ae273e6441862df16a70c2b1
lamthuy305 <lamthuy305@gmail.com>
2022-01-05 20:36:21 +0700

    done

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,36 @@
+package com.codegym;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        int m, n;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Nhập vào số dòng của ma trận: ");
+        m = scanner.nextInt();
+        System.out.println("Nhập vào số cột của ma trận: ");
+        n = scanner.nextInt();
+
+        int arr[][] = new int[m][n];
+
+        System.out.println("Nhập các phần tử cho ma trận: ");
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("arr[" + i + "]["+ j + "] = ");
+                arr[i][j] = scanner.nextInt();
+            }
+        }
+
+        int max = arr[0][0];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (max < arr[i][j]) {
+                    max = arr[i][j];
+                }
+            }
+        }
+
+        System.out.println("Phần tử lớn nhất trong ma trận = " + max);
+    }
+}
